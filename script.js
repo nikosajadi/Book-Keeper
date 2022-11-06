16eefcbc149ff7331cab7c5796de4f0f61133661
nikosajadi <na.sajadii.29@gmail.com>
2022-11-06 14:16:35 +0100

    StoreBookmark

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ modalClose.addEventListener ('click', ()=>modalClose.classList.remove);
 // we want to be able to hide the model by clicking anywhere outside of the modal
 //if the target is this model, then we want to remove the show modal class  and unless we dont want do any things
 window.addEventListener ('click', (e)=> (e.target === modal ? modal.classList.remove ('show-modal') : false));
+
+
+//handing Data form Form we gonna use: The prevent defult() event method
+function storeBookmark(e) {
+    e.preventDefault();
+    // we should pull out values
+    const nameValue = websiteNameEL.value ;
+    let  urlValue = websiteUrlEl.value ;
+    // we dont want to have people have to enter HTP slash
+    if (!urlValue.includes('http://', 'https://')) {
+        urlValue = `http://${urlValue}`;
+    }
+
+
+    console.log(nameValue,urlValue); 
+}
+
+
+
+//handing our input data we can use 'submit event' on the form element 
+ //event Listeners
+ bookmarkForm.addEventListener('submit', storeBookmark);
